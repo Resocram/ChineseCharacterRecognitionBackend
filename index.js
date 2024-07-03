d8f0dd40e501cbff0015c7b83af382d1389209ed
Resocram <marcoser2000@gmail.com>
2024-07-02 23:03:11 -0700

    Changed port

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ wss.on('connection', (ws, req) => {
 
 
 
-
-server.listen(process.env.PORT || 5000, () => {
-  console.log('Server started on port 5000');
+let port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
